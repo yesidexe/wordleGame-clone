e55b1a3d5cea517d9b2f098304bdf30c46c404c5
david <davidjuradoyt@gmail.com>
2023-05-08 17:41:09 -0500

    tenia dos veces la s en el keyboardxd

--- src/components/Keyboard/Keyboard.jsx
+++ src/components/Keyboard/Keyboard.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 const keyRows = [
-    ["Q", "W", "E", "R", "T", "Y", "U", "S", "I", "O", "P"],
+    ["Q", "W", "E", "R", "T", "Y", "U","I", "O", "P"],
     ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
     ["Z", "X", "C", "V", "B", "N", "M"]
 ]
